ce3c69d3d0fc0c37bd5bdd0ac9d5e3de590efdbd
mihail1409 <aleksandrov20012@mail.ru>
2024-05-14 23:51:55 +0300

    2 lab (test.py for check degree result tree)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,8 @@ public class Main {
      public Map<Integer, Pair<Integer, Integer>> notUsedCoordsByVertex = new HashMap<>();
      public List<Pair<Integer, Integer>> edges = new ArrayList<>();
      public int totalWeight = 0;
-     public static int DIAMETER = 128/16;
+     public int longestEdge = 0;
+     public static int DIAMETER = 64/16;
 
     public List<Pair<Integer, Integer>> getEdges() {
         return edges;
@@ -33,7 +34,7 @@ public class Main {
 //         int totalVertex = 64;
 //         int diametr = totalVertex/16;
          List<String> list = new ArrayList<>();
-         BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/Taxicab_128.txt"));
+         BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/Taxicab_4096.txt"));
          String line;
          while ((line = reader.readLine()) != null) {
              list.add(line);
@@ -49,6 +50,7 @@ public class Main {
              coordsByVertex.put(i, Pair.of(Integer.parseInt(splitEl[0]), Integer.parseInt(splitEl[1])));
              i++;
          }
+        System.out.println("asd " +coordsByVertex);
          for (var entry : coordsByVertex.entrySet()) {
              notUsedCoordsByVertex.put(entry.getKey(), entry.getValue());
          }
@@ -57,14 +59,162 @@ public class Main {
 
     public void getTree() throws IOException {
          readFromFile();
-         int del = 11;
-         usedCoordsByVertex.put(del, coordsByVertex.get(del));
-         notUsedCoordsByVertex.remove(del);
+//         int del = 1;
+//         usedCoordsByVertex.put(del, coordsByVertex.get(del));
+//         notUsedCoordsByVertex.remove(del);
+        int point = 1;
+       endAlgorithm(point, coordsByVertex.get(point));
          for (var entry : coordsByVertex.entrySet()) {
-            if (!usedCoordsByVertex.containsKey(entry.getKey())) {
+          //  if (!usedCoordsByVertex.containsKey(entry.getKey())) {
                 //int vertex = entry.getKey();
-                getVertexWithMinWeight(entry.getKey(), entry.getValue());
+                endAlgorithm(entry.getKey(), entry.getValue());
+           // }
+        }
+    }
+
+    private void  test(int vertex, Pair<Integer, Integer> coordsVertex) {
+        ArrayList<Integer> potentialBestVertexs = new ArrayList<>();
+        potentialBestVertexs.add(5);
+        potentialBestVertexs.add(3);
+        potentialBestVertexs.add(10);
+        System.out.println("AA" + potentialBestVertexs);
+        potentialBestVertexs.remove(Collections.max(potentialBestVertexs));
+        System.out.println("KO"+potentialBestVertexs);
+    }
+
+
+    private void endAlgorithm(int vertex, Pair<Integer, Integer> coordsVertex) {
+        if (usedCoordsByVertex.containsKey(vertex)) {
+            return;
+        }
+        ArrayList<Integer> potentialBestVertexs = new ArrayList<>();
+        Map<Integer, Integer> resByPotentialBestVertex = new HashMap<>();
+        // System.out.println("AA" + Collections.min(potentialBestVertexs) + "ad" + Collections.max(potentialBestVertexs));
+        int dist = 99999;
+        int resVertex = vertex;
+        Pair<Integer, Integer> resCoords = coordsVertex;
+        notUsedCoordsByVertex.remove(vertex);
+        for (var entry : notUsedCoordsByVertex.entrySet()) {
+            int neededDegree = getVertexDegree(entry.getKey());
+//            if (vertex == 56) {
+//                System.out.println("teleport");
+//            }
+//            if (entry.getKey() == 57) {
+//                System.out.println(vertex);
+//                System.out.println("rly" +neededDegree);
+//            }
+            int sourceDegree = getVertexDegree(vertex);
+            //System.out.println(neededDegree);
+            var notUsedVertex = entry.getValue();
+            var res = Math.abs(coordsVertex.getLeft() - notUsedVertex.getLeft())
+                    + Math.abs(coordsVertex.getRight() - notUsedVertex.getRight());
+           // System.out.println("res for " + vertex + " and " + entry.getKey() + " is a " + res);
+            //System.out.println("ad " + potentialBestVertexs + " sda " + neededDegree);
+            if (neededDegree != 0 && resByPotentialBestVertex.size() < sourceDegree) {
+//            if (resByPotentialBestVertex.size() < neededDegree && resByPotentialBestVertex.size() < sourceDegree) {
+                resByPotentialBestVertex.put(entry.getKey(), res);
+               // potentialBestVertexs.add(entry.getKey());
+                //System.out.println("size " +potentialBestVertexs.size());
+                //System.out.println();
+            } else if (!resByPotentialBestVertex.isEmpty() && Collections.max(resByPotentialBestVertex.values()) > res && neededDegree != 0 && sourceDegree !=0) { //Collections.max(potentialBestVertexs) > res) {
+                //System.out.println("size " +resByPotentialBestVertex.size());
+              //  if (isNeededVertexDegree(entry.getKey())) {
+                //System.out.println("key"+entry.getKey());
+                for (var resByVertex : resByPotentialBestVertex.entrySet()) {
+                    if (resByVertex.getValue().equals(Collections.max(resByPotentialBestVertex.values()))) {
+                        resByPotentialBestVertex.remove(resByVertex.getKey());
+                        break;
+                    }
+                }
+                resByPotentialBestVertex.put(entry.getKey(), res);
+               // potentialBestVertexs.remove(Collections.max(potentialBestVertexs));
+               // potentialBestVertexs.add(entry.getKey());
+               // System.out.println("aaa " + potentialBestVertexs);
+             //   }
+
             }
+        }
+       //System.out.println("kva");
+        //System.out.println("pot best" + potentialBestVertexs);
+        if (!resByPotentialBestVertex.isEmpty()) {
+            if (resByPotentialBestVertex.containsKey(60)) {
+                System.out.println("haha" +resByPotentialBestVertex);
+            }
+            for (var resByVertex : resByPotentialBestVertex.entrySet()) {
+               // System.out.println("add");
+                var notUsedVertex = notUsedCoordsByVertex.get(resByVertex.getKey());
+              //  System.out.println("vert" + notUsedVertex +"asd" + vert);
+                var res = Math.abs(coordsVertex.getLeft() - notUsedVertex.getLeft())
+                        + Math.abs(coordsVertex.getRight() - notUsedVertex.getRight());
+                totalWeight += res;
+                edges.add(Pair.of(vertex, resByVertex.getKey()));
+                if (longestEdge < res) {
+                    longestEdge = res;
+                }
+            }
+        }
+        usedCoordsByVertex.put(vertex, coordsVertex);
+    }
+
+    private void NEWgetVertexWithMinWeight(int vertex, Pair<Integer, Integer> coordsVertex) {
+        if (usedCoordsByVertex.containsKey(vertex)) {
+            return;
+        }
+        ArrayList<Integer> potentialBestVertexs = new ArrayList<>();
+        // System.out.println("AA" + Collections.min(potentialBestVertexs) + "ad" + Collections.max(potentialBestVertexs));
+        int dist = 99999;
+        int resVertex = vertex;
+        Pair<Integer, Integer> resCoords = coordsVertex;
+        notUsedCoordsByVertex.remove(vertex);
+        for (var entry : notUsedCoordsByVertex.entrySet()) {
+            var notUsedVertex = entry.getValue();
+            var res = Math.abs(coordsVertex.getLeft() - notUsedVertex.getLeft())
+                    + Math.abs(coordsVertex.getRight() - notUsedVertex.getRight());
+            if (potentialBestVertexs.size() != 3) {
+                potentialBestVertexs.add(res);
+            } else if (Collections.max(potentialBestVertexs) > res) {
+                if (isNeededVertexDegree(entry.getKey())) {
+                    potentialBestVertexs.remove(Collections.max(potentialBestVertexs));
+                    potentialBestVertexs.add(entry.getKey());
+                }
+
+            }
+
+        }
+
+    }
+
+    private int getVertexDegree(int checkVertex) {
+        int count = 3;
+        if (edges.isEmpty()) {
+            return count;
+        }
+        for (var pair : edges) {
+            if (pair.getLeft() == checkVertex || pair.getRight() == checkVertex) {
+                count--;
+                if (count == 0) {
+                    return count;
+                }
+            }
+        }
+        return count;
+    }
+
+    private boolean isNeededVertexDegree(int checkVertex) {
+        if (edges.isEmpty()) {
+            return true;
+        }
+        int count = 0;
+        for (var pair : edges) {
+            if (pair.getLeft() == checkVertex || pair.getRight() == checkVertex) {
+                count++;
+            }
+        }
+        if (count == 3) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
 
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -12,8 +12,10 @@ public class Runner {
         //System.out.println(mainOgj.getUsedCoordsByVertex());
        // System.out.println(mainOgj.getNotUsedCoordsByVertex());
         System.out.println(mainOgj.getTotalWeight());
-//        FileWriter writer = new FileWriter("Aleksandrov_512_3.txt");
-//        writer.write("c Вес дерева = " + mainOgj.getTotalWeight() + " диаметр = " + System.getProperty("line.separator"));
+        System.out.println(mainOgj.longestEdge);
+//
+//        FileWriter writer = new FileWriter("Aleksandrov_64_1.txt");
+//        writer.write("c Вес кубического подграфа = " + mainOgj.getTotalWeight() + " самое длинное ребро = " + mainOgj.longestEdge + "," + System.getProperty("line.separator"));
 //        writer.write("p edge = " + mainOgj.getUsedCoordsByVertex().size() + " " + mainOgj.getEdges().size() + System.getProperty("line.separator") );
 //
 //        for(var edge : edges) {
@@ -22,5 +24,19 @@ public class Runner {
 //            writer.write("e " + left + " " + right + System.getProperty("line.separator"));
 //        }
 //        writer.close();
+//
+//        int bestRes = 0;
+//        int total = 0;
+//        for (var pair : mainOgj.getEdges()) {
+//            var firstVertex = mainOgj.coordsByVertex.get(pair.getLeft());
+//            var secondVertex = mainOgj.coordsByVertex.get(pair.getRight());
+//            var res = Math.abs(firstVertex.getLeft() - secondVertex.getLeft())
+//                    + Math.abs(firstVertex.getRight() - secondVertex.getRight());
+//            total +=res;
+//            if (res > bestRes) {
+//                bestRes = res;
+//            }
+//        }
+//        System.out.println(mainOgj.longestEdge + " самое длинное ребро " + bestRes + "total weight" +total);
     }
 }
